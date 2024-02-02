56b4af16b421d2f43fabf80283a136358236f0ee
Allan_A <109562531+AllanA2k@users.noreply.github.com>
2024-02-01 23:06:45 -0500

    Update sqlite.py
    added the update_user_income function to update the main.py changes.
    also added taxable_amount trying to commit the marital addition

--- sqlite.py
+++ sqlite.py
@@ -8,6 +8,7 @@ def create_table():
         c.execute("""CREATE TABLE IF NOT EXISTS users (
             first text,
             last text,
+            taxable real,
             total_income real,
             tax real
             )""")
@@ -20,11 +21,19 @@ def create_table():
 def add_user(user):
     conn = sqlite3.connect('user.db')
     c = conn.cursor()
-    c.execute("INSERT INTO users VALUES (:first, :last, :total_income, :tax)",
-              (user.first, user.last, user.total_income, user.tax))
+    c.execute("INSERT INTO users VALUES (:first, :last, :taxable_amount :total_income, :tax)",
+              (user.first, user.last, user.taxable_amount, user.total_income, user.tax))
     conn.commit()
     conn.close()
 
+def update_user_income(first_name, last_name, new_income):
+    conn = sqlite3.connect('user.db')
+    c = conn.cursor()
+    c.execute("UPDATE users SET total_income = ?, tax = ? WHERE first = ? AND last = ?",
+              (new_income, round((new_income - 12750) * 0.0475, 2), first_name, last_name))
+
+    conn.commit()
+    conn.close()
 
 def display_table():
     conn = sqlite3.connect('user.db')
