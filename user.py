c9fdabe5a496f078806c47eb8e16b72e9848372b
Allan_A <109562531+AllanA2k@users.noreply.github.com>
2024-02-01 23:08:25 -0500

    Update user.py
    added taxable_amouint self.
    added some changes to the tax amount depending on the marital status

--- user.py
+++ user.py
@@ -1,12 +1,17 @@
 class User:
 
-    def __init__(self, first, last, total_income):
+    def __init__(self, first, last, taxable_amount, total_income):
         self.first = first
         self.last = last
+        self.taxable_amount = taxable_amount
         self.total_income = total_income
         # https://www.ncdor.gov/taxes-forms/individual-income-tax/north-carolina-standard-deduction-or-north-carolina-itemized-deductions
         # standard deduction for single status is $12,750.
         # income tax rate is 4.75%
-        self.tax = round((total_income - 12750) * 0.0475, 2)
+        
+        standard_deduction = 12750 if taxable_amount == "Single" else 0
+        taxable_income = max(0, total_income - standard_deduction)
+        
+        self.tax = round(taxable_income * 0.0475, 2)
 
 
